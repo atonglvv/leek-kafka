b72e2ff7e10c2165c64f74d111e016bb3122e00d
atongTs <atongLv@163.com>
2021-06-16 22:18:47 +0800

    feat leek-kafka-api hello Kafka

---
+++ leek-kafka-api/src/main/java/cn/atong/leek/kafka/api/hello/HelloConsumer.java
@@ -0,0 +1,47 @@
+package cn.atong.leek.kafka.api.hello;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+/**
+ * @program: leek-kafka
+ * @description: Hello Consumer
+ * @author: atong
+ * @create: 2021-06-16 21:09
+ */
+public class HelloConsumer {
+
+    public static void main(String[] args) {
+        //Consumer 必须指定三个参数：
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "127.0.0.1:9092");
+        properties.put("key.deserializer", StringDeserializer.class);
+        properties.put("value.deserializer", StringDeserializer.class);
+        // group.id 并非完全必须指定
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "hellogroup");
+
+        KafkaConsumer<String,String> consumer = new KafkaConsumer<String, String>(properties);
+
+        try {
+            //消费者订阅主题, 可以订阅多个
+            consumer.subscribe(Collections.singleton("HelloTopic"));
+            while (true) {
+                // 拉取消息
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(500));
+                for (ConsumerRecord<String, String> recode : records) {
+                    System.out.println(String.format("topic:%s,分区：%d,偏移量：%d," + "key:%s,value:%s",
+                            recode.topic(), recode.partition(), recode.offset(), recode.key(), recode.value()));
+                }
+            }
+        }finally {
+            consumer.close();
+        }
+    }
+}
---
+++ leek-kafka-api/src/main/java/cn/atong/leek/kafka/api/hello/HelloProducer.java
@@ -0,0 +1,42 @@
+package cn.atong.leek.kafka.api.hello;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+/**
+ * @program: leek-kafka
+ * @description: Hello Producer 最简单的 Kafka Producer
+ * @author: atong
+ * @create: 2021-06-16 20:09
+ */
+public class HelloProducer {
+
+    public static void main(String[] args) {
+        //Producer 必须指定的三个属性： bootstrap.servers; key.serializer; value.serializer
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "127.0.0.1:9092");
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        KafkaProducer<String,String> producer = new KafkaProducer<String, String>(properties);
+
+        try{
+            ProducerRecord<String,String> record;
+            try{
+                //发送四条消息
+                for (int i = 0; i < 6; i++) {
+                    record = new ProducerRecord<String, String>("HelloTopic", String.valueOf(i), "hello kafka");
+                    //三种发送方式之一 : 简单发送, 发送即忘
+                    producer.send(record);
+                    System.out.println("发送第" + i + "条消息...");
+                }
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }finally {
+            producer.close();
+        }
+
+    }
+}
