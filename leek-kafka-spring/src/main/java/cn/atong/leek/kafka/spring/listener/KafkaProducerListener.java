68bfb6de43e1dbcfeab366459a4a0be4ae66a2b2
atongTs <atongLv@163.com>
2021-06-21 23:16:11 +0800

    feat KafkaProducerListener/kafkaTemplate

---
+++ leek-kafka-spring/src/main/java/cn/atong/leek/kafka/spring/listener/KafkaProducerListener.java
@@ -0,0 +1,12 @@
+package cn.atong.leek.kafka.spring.listener;
+
+import org.springframework.kafka.support.ProducerListener;
+
+/**
+ * @program: leek-kafka
+ * @description: KafkaProducerListener
+ * @author: atong
+ * @create: 2021-06-21 23:14
+ */
+public class KafkaProducerListener implements ProducerListener {
+}
